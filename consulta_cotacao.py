5c0c46cabad01d8332981c4cb94408ac9fdaf598
João Paulo <79981086+JoaoPaulo-creator@users.noreply.github.com>
2022-03-09 15:48:57 -0300

    Update consulta_cotacao.py

--- consulta_cotacao.py
+++ consulta_cotacao.py
@@ -1,21 +1,29 @@
 from httpx import get
+from tratando_requisicao import verificando_status_erro_requisicao
 
 
 def consulta_cotacao_moeda(link_consulta):
-    requisicao = get(link_consulta, timeout=None)
-    saida_em_json = requisicao.json()['USDBRL']
+    USD = 'USD'
+    BRL = 'BRL'
+    PARES = f'{USD}-{BRL}'
+    PARES_JSON = f'{USD}{BRL}'
 
-    um_dolar_em_real = saida_em_json['ask']
-    valor_cotado = saida_em_json['ask']
-    maxima_do_dia = saida_em_json['high']
-    minima_do_dia = saida_em_json['low']
-    variacao = saida_em_json['varBid']
+    requisicao = get(f'{link_consulta}/{PARES}', timeout=None)
 
-    output = f'\n COTAÇÃO DO DÓLAR AMERICANO \n' \
-             f'\nUSD 1 = R$ {um_dolar_em_real}\n' \
-             f'Valor cotado: {valor_cotado}\n' \
-             f'Maxima do dia: {maxima_do_dia}\n' \
-             f'Mínima do dia: {minima_do_dia}\n' \
-             f'Variação: {variacao}'
+    if verificando_status_erro_requisicao(requisicao):
+        verificando_status_erro_requisicao(requisicao)
+    else:
+        saida_em_json = requisicao.json()[f'{PARES_JSON}']
+        valor_cotado = saida_em_json['ask']
+        maxima_do_dia = saida_em_json['high']
+        minima_do_dia = saida_em_json['low']
+        variacao = saida_em_json['varBid']
 
-    return output
+        output = f'\n COTAÇÃO DO DÓLAR AMERICANO \n' \
+                 f'\nUSD 1 = R$ {valor_cotado}\n' \
+                 f'Valor cotado: {valor_cotado}\n' \
+                 f'Maxima do dia: {maxima_do_dia}\n' \
+                 f'Mínima do dia: {minima_do_dia}\n' \
+                 f'Variação: {variacao}'
+
+        return output
